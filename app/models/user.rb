ef1d27e88259a9d360db51af0c9bae69f751f550
Michal Gajowiak <m.gajowiak@nomtek.com>
2018-01-24 12:47:37 +0100

    Add rubocop and fix offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :name, length: { in: 2..30 }
-  validates :surname, length: {in: 2.. 40 }
+  validates :surname, length: { in: 2..40 }
   validates :birthdate, presence: true
 end
